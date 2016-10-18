1b534e70c56d22d97f55cf5de2660d23dd71f696
slayerten <slayerten@163.com>
2016-10-19 07:26:41 +0800

    the first file  请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

---
+++ demo.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+
+    def print_demo(self):
+        print '%s:%s:'%(self.name,self.score),self.print_aa()
+        
+
+    def print_aa(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 70:
+            return 'B'
+        else:
+            return 'C'
+
+Wolf = Student('Wolf Cui',99)
+John = Student('John Lily',86)
+Q = Student('Q Yang',55)
+Wolf.print_demo()
+John.print_demo()
+Q.print_demo()
